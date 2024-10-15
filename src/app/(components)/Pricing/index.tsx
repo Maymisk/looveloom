99732f3942828d6f874245ad752b99674a408048
Khalil Bohner <khalilbohner@gmail.com>
2024-10-14 21:57:14 -0300

    feat(landing_page): Continua o desenvolvimento da landing page :maple_leaf:

--- src/app/(components)/Pricing/index.tsx
+++ src/app/(components)/Pricing/index.tsx
@@ -1,64 +1,68 @@
-import { Check, X } from 'lucide-react';
-import { PricingCard } from './card';
+import { Check, X } from "lucide-react";
+import { PricingCard } from "./card";
+import { ShineBorder } from "@/shared/components/shine-border";
 
 export function LandingPagePricing() {
-	return (
-		<section className="flex max-xl:flex-col items-center justify-center gap-6 max-xl:gap-12">
-			<PricingCard title="Standard" price={5} oldPrice={7}>
-				<ul className="flex flex-col gap-3">
-					<li className="flex gap-2 items-center">
-						<Check className="text-green-700 w-5 h-5 mb-1" />
-						<span>1 year acess</span>
-					</li>
+  return (
+    <section className="flex max-xl:flex-col items-center justify-center gap-6 max-xl:gap-12">
+      <PricingCard title="Standard" price={5} oldPrice={7}>
+        <ul className="flex flex-col gap-3">
+          <li className="flex gap-2 items-center">
+            <Check className="text-green-700 w-5 h-5 mb-1" />
+            <span>1 year acess</span>
+          </li>
 
-					<li className="flex gap-2 items-center">
-						<Check className="text-green-700 w-5 h-5 mb-1" />
-						<span>3 pictures</span>
-					</li>
+          <li className="flex gap-2 items-center">
+            <Check className="text-green-700 w-5 h-5 mb-1" />
+            <span>3 pictures</span>
+          </li>
 
-					<li className="flex gap-2 items-center">
-						<X className="text-red-700 w-5 h-5 mb-1" />
-						<span>With music</span>
-					</li>
+          <li className="flex gap-2 items-center">
+            <X className="text-red-700 w-5 h-5 mb-1" />
+            <span>With music</span>
+          </li>
 
-					<li className="flex gap-2 items-center">
-						<X className="text-red-700 w-5 h-5 mb-1" />
-						<span>Relationship milestones</span>
-					</li>
-				</ul>
-			</PricingCard>
+          <li className="flex gap-2 items-center">
+            <X className="text-red-700 w-5 h-5 mb-1" />
+            <span>Relationship milestones</span>
+          </li>
+        </ul>
+      </PricingCard>
 
-			<div className="relative w-full max-w-[400px] shadow-md shadow-gray-800">
-				<div className="w-max absolute -top-3 left-1/2 -translate-x-1/2 text-xl text-center font-bold capitalize bg-red-500 rounded-full px-4 ">
-					Most popular deal
-				</div>
+      <div className="relative w-full max-w-[400px]">
+        <div className="w-max absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-black text-xl text-center font-bold capitalize rounded-full px-4 py-1">
+          <span className="text-red-500">Best deal</span>
+        </div>
 
-				<div className=" flex flex-col justify-center p-[1px] bg-default-gradient rounded-lg">
-					<PricingCard title="Premium" oldPrice={15} price={10}>
-						<ul className="flex flex-col gap-3">
-							<li className="flex gap-2 items-center">
-								<Check className="text-green-700 w-5 h-5 mb-1" />
-								<span>Lifetime access</span>
-							</li>
+        <ShineBorder
+          className="relative -z-10 w-full flex flex-col justify-center text-white p-[1px] bg-default-gradient rounded-lg"
+          color={["#A07CFE", "#FE8FB5", "#FFBE7B"]}
+        >
+          <PricingCard title="Premium" oldPrice={15} price={10}>
+            <ul className="flex flex-col gap-3">
+              <li className="flex gap-2 items-center">
+                <Check className="text-green-700 w-5 h-5 mb-1" />
+                <span>Lifetime access</span>
+              </li>
 
-							<li className="flex gap-2 items-center">
-								<Check className="text-green-700 w-5 h-5 mb-1" />
-								<span>7 pictures</span>
-							</li>
+              <li className="flex gap-2 items-center">
+                <Check className="text-green-700 w-5 h-5 mb-1" />
+                <span>7 pictures</span>
+              </li>
 
-							<li className="flex gap-2 items-center">
-								<Check className="text-green-700 w-5 h-5 mb-1" />
-								<span>With music</span>
-							</li>
+              <li className="flex gap-2 items-center">
+                <Check className="text-green-700 w-5 h-5 mb-1" />
+                <span>With music</span>
+              </li>
 
-							<li className="flex gap-2 items-center">
-								<Check className="text-green-700 w-5 h-5 mb-1" />
-								<span>Relationship milestones</span>
-							</li>
-						</ul>
-					</PricingCard>
-				</div>
-			</div>
-		</section>
-	);
+              <li className="flex gap-2 items-center">
+                <Check className="text-green-700 w-5 h-5 mb-1" />
+                <span>Relationship milestones</span>
+              </li>
+            </ul>
+          </PricingCard>
+        </ShineBorder>
+      </div>
+    </section>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,102 +1,119 @@
-import type { Config } from 'tailwindcss';
-import { fontFamily } from 'tailwindcss/defaultTheme';
+import type { Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 const config = {
-	darkMode: ['class'],
-	content: [
-		'./pages/**/*.{ts,tsx}',
-		'./components/**/*.{ts,tsx}',
-		'./app/**/*.{ts,tsx}',
-		'./src/**/*.{ts,tsx}',
-	],
-	prefix: '',
-	theme: {
-		container: {
-			center: true,
-			padding: '2rem',
-			screens: {
-				'2xl': '1400px',
-			},
-		},
-		extend: {
-			colors: {
-				gray: {
-					50: '#4B4B4B',
-					100: '#696969',
-					200: '#7D7D7D',
-					300: '#A0A0A0',
-					400: '#B5B5B5',
-					500: '#282424',
-					600: '#DCDCDC',
-					700: '#EAEAEA',
-					800: '#1A1C1C',
-				},
-
-				red: {
-					50: '#F8D1D5',
-					100: '#F3B3B9',
-					200: '#E8959D',
-					300: '#DC143C',
-					400: '#C65A65',
-					500: '#B53D49',
-					600: '#A22431',
-					700: '#8E1C28',
-					800: '#7A141F',
-				},
-				ivory: {
-					400: '#FFD58D',
-					500: '#FFF8E1', // DEFAULT
-				},
-				pink: {
-					50: '#FFF5FF',
-					100: '#FFE4FF',
-					200: '#FFD3FF',
-					300: '#FFC2FF',
-					400: '#CCAACC',
-					500: '#FFD5FF', // DEFAULT
-					600: '#997F99',
-					700: '#665566',
-					800: '#332A33',
-				},
-				sage: {
-					50: '#E8F1ED',
-					100: '#D1E3DB',
-					200: '#BBC5C9',
-					300: '#A4D6B6',
-					400: '#6F9088',
-					500: '#8BB8A8', // DEFAULT
-					600: '#556C68',
-					700: '#3A4847',
-					800: '#1F2423',
-				},
-			},
-
-			backgroundImage: {
-				'default-gradient': 'linear-gradient(135deg, #DC143C, #FFFFFF)',
-			},
-
-			keyframes: {
-				'accordion-down': {
-					from: { height: '0' },
-					to: { height: 'var(--radix-accordion-content-height)' },
-				},
-				'accordion-up': {
-					from: { height: 'var(--radix-accordion-content-height)' },
-					to: { height: '0' },
-				},
-			},
-
-			animation: {
-				'accordion-down': 'accordion-down 0.2s ease-out',
-				'accordion-up': 'accordion-up 0.2s ease-out',
-			},
-		},
-		fontFamily: {
-			playfair: ['var(--playfair)', ...fontFamily.serif],
-			poppins: ['var(--poppins)', ...fontFamily.serif],
-		},
-	},
-	plugins: [require('tailwindcss-animate')],
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  prefix: "",
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          "50": "#4B4B4B",
+          "100": "#696969",
+          "200": "#7D7D7D",
+          "300": "#A0A0A0",
+          "400": "#B5B5B5",
+          "500": "#282424",
+          "600": "#DCDCDC",
+          "700": "#EAEAEA",
+          "800": "#1A1C1C",
+        },
+        red: {
+          "50": "#F8D1D5",
+          "100": "#F3B3B9",
+          "200": "#E8959D",
+          "300": "#DC143C",
+          "400": "#C65A65",
+          "500": "#B53D49",
+          "600": "#A22431",
+          "700": "#8E1C28",
+          "800": "#7A141F",
+        },
+        lavender: {
+          "500": "#E6C8FA",
+        },
+        pink: {
+          "50": "#FFF5FF",
+          "100": "#FFE4FF",
+          "200": "#FFD3FF",
+          "300": "#FFC2FF",
+          "400": "#CCAACC",
+          "500": "#FFD5FF",
+          "600": "#997F99",
+          "700": "#665566",
+          "800": "#332A33",
+        },
+        sage: {
+          "50": "#E8F1ED",
+          "100": "#D1E3DB",
+          "200": "#BBC5C9",
+          "300": "#A4D6B6",
+          "400": "#6F9088",
+          "500": "#8BB8A8",
+          "600": "#556C68",
+          "700": "#3A4847",
+          "800": "#1F2423",
+        },
+      },
+      backgroundImage: {
+        "default-gradient": "linear-gradient(135deg, #DC143C, #FFFFFF)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
+        },
+        shine: {
+          "0%": {
+            "background-position": "0% 0%",
+          },
+          "50%": {
+            "background-position": "100% 100%",
+          },
+          to: {
+            "background-position": "0% 0%",
+          },
+        },
+        "shiny-text": {
+          "0%, 90%, 100%": {
+            "background-position": "calc(-100% - var(--shiny-width)) 0",
+          },
+          "30%, 60%": {
+            "background-position": "calc(100% + var(--shiny-width)) 0",
+          },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        shine: "shine var(--duration) infinite linear",
+        "shiny-text": "shiny-text 8s infinite",
+      },
+    },
+    fontFamily: {
+      playfair: ["var(--playfair)", ...fontFamily.serif],
+      poppins: ["var(--poppins)", ...fontFamily.serif],
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
 
 export default config;
