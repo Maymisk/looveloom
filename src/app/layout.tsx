cec0ce924cb205adde9ee80ca7dc90a71465028e
Khalil Bohner <khalilbohner@gmail.com>
2025-02-13 16:48:54 -0300

    :maple_leaf: Adds FB pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { Poppins } from 'next/font/google';
 import dbConnect from './api/mongo';
 import './globals.css';
 import { Footer } from '@/shared/components/footer';
+import Script from 'next/script';
 
 const poppins = Poppins({
 	subsets: ['latin'],
@@ -60,6 +61,35 @@ export default async function RootLayout({
 
 	return (
 		<html lang="en">
+			<head>
+				<Script
+					id="fb-pixel"
+					strategy="afterInteractive"
+					dangerouslySetInnerHTML={{
+						__html: `
+					!function(f,b,e,v,n,t,s)
+					{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+					n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+					if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+					n.queue=[];t=b.createElement(e);t.async=!0;
+					t.src=v;s=b.getElementsByTagName(e)[0];
+					s.parentNode.insertBefore(t,s)}(window, document,'script',
+					'https://connect.facebook.net/en_US/fbevents.js');
+					fbq('init', '1367157014282158');
+					fbq('track', 'PageView');
+				`,
+					}}
+				/>
+
+				<noscript>
+					<img
+						height="1"
+						width="1"
+						style={{ display: 'none' }}
+						src="https://www.facebook.com/tr?id=1367157014282158&ev=PageView&noscript=1"
+					/>
+				</noscript>
+			</head>
 			<body className={cn('bg-gray-500 text-white', poppins.variable)}>
 				<header className="h-32 flex items-center bg-gray500 shadow-md shadow-gray-800">
 					<Navbar />
